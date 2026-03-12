7a52007ed17474f27373701c71e9d60629f6ebef
Lakshya <lakshyashetty2404@gmail.com>
2026-03-12 11:22:25 +0530

    "new Init"

--- Test/src/com/Main.java
+++ Test/src/com/Main.java
@@ -3,8 +3,6 @@ package com;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Laksh");
-		System.out.println("New Me");
-		System.out.println("Hahaha");
 	}
 
 }
